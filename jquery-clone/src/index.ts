a07df796722b33abbefd5886fc3ea56822524ccf
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-05-15 23:07:56 +0530

    Jquery clone DOM

--- jquery-clone/src/index.ts
+++ jquery-clone/src/index.ts
@@ -1,13 +1,52 @@
 import fetch from "node-fetch";
 
-function $(selector: string): any {
-  return {} as any;
+class SelectorResult {
+  #elements
+  constructor(elements: NodeListOf<Element>) {
+    this.#elements = elements
+  }
+  html(contents: string) {
+    this.#elements.forEach(ele => {
+      ele.innerHTML = contents
+    })
+  }
+  on<K extends keyof HTMLElementEventMap>(eventname: K,
+    arg1: (event: HTMLElementEventMap[K]) => void) {
+    this.#elements.forEach(ele => {
+      const htmlEle = ele as HTMLElement
+      htmlEle.addEventListener(eventname, arg1)
+    })
+  }
+  show() {
+    this.#elements.forEach(ele => {
+      const htmlEle = ele as HTMLElement
+      htmlEle.style.visibility = 'visible'
+    })
+  }
+  hide() {
+    this.#elements.forEach(ele => {
+      const htmlEle = ele as HTMLElement
+      htmlEle.style.visibility = 'hidden'
+    })
+  }
+
+
+}
+
+function $(selector: string) {
+  return new SelectorResult(
+    document.querySelectorAll(selector)
+  )
+
 }
 
 namespace $ {
-  export function ajax(...args: any[]): any {
-    return {} as any;
+  export function ajax(
+    { url, sucuessCb }:
+      { url: string, sucuessCb: (data: any) => void }): any {
+    return fetch(url).then(resp => resp.json()).then(sucuessCb)
   }
 }
 
 export default $;
+
